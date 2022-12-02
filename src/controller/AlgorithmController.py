be8701422a3a2f9b8be3e22f55348fcbe19211e6
Sai Vineeth Kumar Dara <sdara@umass.edu>
2022-12-02 11:44:57 -0500

    Adding Controller files (skeleton structure) to the project

---
+++ src/controller/AlgorithmController.py
@@ -0,0 +1,24 @@
+class AlgorithmController():
+    """
+    Controller Class to compute the shortest path with elevation using the chosen algorithm.
+    """
+
+    def __init__(self):
+        """
+        inti method: initializes the  class attributes
+        """
+
+    def calculate_elevation_path(self):
+        """
+        This method finds the path with nodes
+        """
+
+    def fetch_route_with_elevation(self):
+        """
+        This method finds the elevated path in the graph
+        """
+
+
+
+
+
---
+++ src/controller/RouteController.py
@@ -0,0 +1,23 @@
+class RouteController:
+    """
+    Controller Class to modify the route models
+    """
+    def __init__(self):
+        """
+        inti method: initializes the  class attributes
+        """
+
+    def calculate_shortest_path(self, source, destination):
+        """
+        This method gets the shortest path model based on the starting and ending coordinates.
+        """
+    def calcuate_elevation_path(self):
+        """
+        This method gets the elevation gain path model based on the source and destination coordinates.
+        """
+
+
+    def calculate_final_route(self, start_point, end_point, deviation_percent, minmax_elev_gain):
+        """
+        This method gets the final shortest path model with desired elevation with in path distance deviation limit provided.
+        """
