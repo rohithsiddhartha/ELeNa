889feec7ca56ac59b14f441dea38a8775d91b457
Sai Vineeth Kumar Dara <sdara@umass.edu>
2022-12-02 12:03:18 -0500

    Adding code to controller files' methods

--- src/controller/AlgorithmController.py
+++ src/controller/AlgorithmController.py
@@ -1,23 +1,113 @@
+import math
+import osmnx as ox
+from heapq import heappush, heappop
+from itertools import count
+from networkx.algorithms.shortest_paths.weighted import _weight_function
+from src.model.AlgorithmModel import AlgorithmModel
+from src.model.PathModel import *
+import networkx as nx
+
 class AlgorithmController():
     """
     Controller Class to compute the shortest path with elevation using the chosen algorithm.
     """
 
-    def __init__(self):
+    def __init__(self, graph, shortest_dist, path_limit, elevation_strategy, source, destination, elevation_gain,
+                 algo_flag):
         """
         inti method: initializes the  class attributes
         """
 
-    def calculate_elevation_path(self):
+        self.graph = graph
+        self.origin = source
+        self.destination = destination
+        self.shortest_dist = shortest_dist
+        self.elevation_path = None
+        self.elevation_strategy = elevation_strategy
+        self.elevation_gain = elevation_gain
+        self.path_limit = path_limit
+        self.algo_flag = algo_flag
+        self.model = AlgorithmModel()
+
+    def calculate_elevation_path(self, Graph, source, target, heuristic, weight):
         """
         This method finds the path with nodes
+
+        Parameters
+        ----------
+            Graph: area map as graph with all the points as nodes
+            source: starting point
+            destination: final destination point
         """
+
+        weight = _weight_function(Graph, weight)
+        cnt = count()
+        path_queue = [(0, next(cnt), source, 0, None)]
+        enqueued = {}
+        visited_parent = {}  # storing parent of a visited nodes
+        while path_queue:
+            _, __, current_node, distance, parent_node = heappop(path_queue)
+            if current_node == target:
+                node = parent_node
+                path = [current_node]
+                while node is not None:
+                    path.append(node)
+                    node = visited_parent[node]
+                path.reverse()
+                return path
+
+            visited_parent[current_node] = parent_node
+            for neighbor, w in Graph[current_node].items():
+                cost = distance + weight(current_node, neighbor, w)
+                if neighbor in enqueued:
+                    qcost, h = enqueued[neighbor]
+                    if qcost <= cost:
+                        continue
+                else:
+                    h = heuristic(neighbor, target)
+                enqueued[neighbor] = cost, h
+                heappush(path_queue, (cost + h, next(cnt), neighbor, cost, current_node))
+
+        raise nx.NetworkXNoPath(f"Node {target} not reachable from {source}")
+
 
     def fetch_route_with_elevation(self):
         """
         This method finds the elevated path in the graph
         """
 
+        if self.elevation_strategy == "min":
+            minmax = 1
+        else:
+            minmax = 0
+
+        self.elevation_path = nx.shortest_path(self.graph, source=self.origin, target=self.destination,
+                                               weight="length")
+        heurestic_val = None
+        while self.scaling_factor < 10000:
+            elevation_path = self.calculate_elevation_path(self.graph, source=self.origin, target=self.destination, heuristic=heurestic_val,
+                                                           weight=lambda u, v, d: math.exp(minmax * d[0]["length"] * (d[0]['grade'] + d[0]['grade_abs']) / 2)
+                                                           + math.exp((1 / self.scaling_factor) * d[0]["length"]))
+
+            elevation_distance = sum(ox.utils_graph.get_route_edge_attributes(self.graph, elevation_path, "length"))
+            elevation_gain = self.model.get_path_weight(self.graph, elevation_path, "elevation_gain")
+            if elevation_distance <= (self.path_limit) * self.shortest_dist and \
+                    minmax * elevation_gain <= minmax * self.elevation_gain:
+                self.elevation_path = elevation_path
+                self.elevation_gain = elevation_gain
+            self.scaling_factor = self.scaling_factor*3
+
+        # Configure the path model - setting appropriate attributes
+        path_model = PathModel()
+        path_model.set_algo(str(self.algo_flag))
+        path_model.set_elevation_gain(self.model.get_path_weight(self.graph, self.elevation_path, "elevation_gain"))
+        path_model.set_drop(0)
+        path_model.set_path([[self.graph.nodes[route_node]['x'], self.graph.nodes[route_node]['y']]
+                             for route_node in self.elevation_path])
+        path_model.set_distance(sum(ox.utils_graph.get_route_edge_attributes(self.graph, self.elevation_path, "length")))
+        path_model.set_path_flag(2)
+
+        return path_model
 
 
 
--- src/controller/RouteController.py
+++ src/controller/RouteController.py
@@ -1,23 +1,62 @@
+from src.model.AlgorithmModel import AlgorithmModel
+from src.model.MapGraphModel import MapGraphModel
+from src.controller.ShortestPathController import ShortestPathController
+from src.controller.AlgorithmController import AlgorithmController
+
 class RouteController:
     """
     Controller Class to modify the route models
     """
     def __init__(self):
         """
-        inti method: initializes the  class attributes
+        init method: initializes the  class attributes - algorithm_model, shortest_path, elevation_path
         """
+        self.algorithm_model = AlgorithmModel()
+        self.shortest_path = None
+        self.elevation_path = None
 
     def calculate_shortest_path(self, source, destination):
         """
         This method gets the shortest path model based on the starting and ending coordinates.
         """
+
+        self.algorithm_model.set_graph(MapGraphModel().get_map_graph(destination))
+        shortestPathController = ShortestPathController(self.algorithm_model.get_graph())
+        return shortestPathController.get_shortest_path(source, destination)
+
     def calcuate_elevation_path(self):
         """
         This method gets the elevation gain path model based on the source and destination coordinates.
         """
 
+        algorithmController = AlgorithmController(self.algorithm_model.get_graph(), self.shortest_path.get_distance(),
+                                    self.algorithm_model.get_path_limit(), self.algorithm_model.get_elevation_strategy(),
+                                    self.shortest_path.get_origin(), self.shortest_path.get_destination(),
+                                    self.shortest_path.get_gain(), self.algorithm_model.get_algo_flag())
+        return algorithmController.fetch_route_with_elevation()
 
-    def calculate_final_route(self, start_point, end_point, deviation_percent, minmax_elev_gain):
+    def print_route_attributes(self, path):
+        """
+        Displays the information about the path.
+        """
+
+
+    def calculate_final_route(self, start_point, end_point, deviation_percent, minmax_elev_gain, map_view):
         """
         This method gets the final shortest path model with desired elevation with in path distance deviation limit provided.
         """
+
+        self.shortest_path = self.calculate_shortest_path(start_point, end_point)
+        self.print_route_attributes(self.shortest_path)
+
+        self.algorithm_model.set_path_limit(float(deviation_percent))
+        self.algorithm_model.set_elevation_strategy(minmax_elev_gain)
+        self.algorithm_model.set_algo_flag(2)
+
+        self.elevation_path = self.calcuate_elevation_path()
+        self.print_route_attributes(self.elevation_path)
+
+        self.shortest_path.register(map_view)
+        self.shortest_path.state_changed()
+        self.elevation_path.register(map_view)
+        self.elevation_path.state_changed()
