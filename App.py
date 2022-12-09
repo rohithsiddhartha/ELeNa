e97aee1d899e7f772869ba10b8a5b6d174e906ab
tush05tgsingh <tushtgsingh.05@gmail.com>
2022-12-08 22:43:20 -0500

    added map view function

--- App.py
+++ App.py
@@ -1,11 +1,11 @@
 # Entry point for the application.
-from src.view.MapView import MapView
+
 from . import app
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime
 import googlemaps
 from src.controller.RouteController import RouteController
-import json
+from src.view.view import MapView
 
 gmaps = googlemaps.Client(key='AIzaSyCC19S4I6tEZYE9Iv4zhsNRixctc6gp_Dc')
 
@@ -20,22 +20,21 @@ def index(request):
 		request = request.replace("%2C", "," )
 		request = request.replace("%20", " " )
 		start_location, end_location, percentage, minmax_elev_gain = request.split(":")
-		#, algo = request.split(":")
 		print("Input Parameters")
 		print(start_location)
 		print(end_location)
 		print(percentage)
 		print(minmax_elev_gain)
-		#print(algo)
+		
 
 		start_coordinates = [gmaps.geocode(start_location)[0]['geometry']['location']['lat'], gmaps.geocode(start_location)[0]['geometry']['location']['lng']]
 		end_coordinates = [gmaps.geocode(end_location)[0]['geometry']['location']['lat'], gmaps.geocode(end_location)[0]['geometry']['location']['lng']]
 
 		view = MapView()
 		controller = RouteController()
-		#controller.calculate_final_route(start_coordinates, end_coordinates, percent, minmax_elev_gain, algo, view)
+		
 		controller.calculate_final_route(start_coordinates, end_coordinates, percentage, minmax_elev_gain, view)
-		path_coordinates, total_distance, elevation = view.get_route_params()
+		path_coordinates, total_distance, calculated_elevation = view.get_route_params()
 		path_coordinates = [i[::-1] for i in path_coordinates]
 		path = [[]]
 		route_cds = []
@@ -49,5 +48,5 @@ def index(request):
 				path.append([])
 			path[-1].append(coordinate)
 			c += 1
-		print("Elevation:", elevation)
-	return jsonify(origin=start_coordinates, des=end_coordinates,path=path, dis=total_distance, elev=elevation)
+		print("Elevation:", calculated_elevation)
+	return jsonify(origin=start_coordinates, des=end_coordinates,path=path, dis=total_distance, elev=calculated_elevation)
